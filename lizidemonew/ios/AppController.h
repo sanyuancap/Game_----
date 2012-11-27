14aa222e4d8579e48061d1f634f7a2c983128fae
gq <apple@applematoMacBook-Pro.local>
2012-11-27 11:00:42 +0800

    Initial Commit

---
+++ lizidemonew/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  lizidemonewAppController.h
+//  lizidemonew
+//
+//  Created by gq on 12-11-27.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ lizidemonew/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  lizidemonewAppController.h
+//  lizidemonew
+//
+//  Created by gq on 12-11-27.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
